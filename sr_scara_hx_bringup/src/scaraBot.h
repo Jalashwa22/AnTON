c4782165eac43a6c36d16fabb341ec0de092168a
Jalashwa22 <sagnik.basuchoudhuri@outlook.com>
2017-08-30 00:42:59 +0200

    Enhancement to the Write Function in the  HW Interface File

--- sr_scara_hx_bringup/src/scaraBot.h
+++ sr_scara_hx_bringup/src/scaraBot.h
@@ -2,62 +2,124 @@
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/robot_hw.h>
 #include <iostream>
+#include <string>
+#include <sstream>
+
+namespace patch
+{
+template < typename T > std::string to_string( const T& n )
+{
+    std::ostringstream stm ;
+    stm << n ;
+    return stm.str() ;
+}
+}
+
+
+using namespace patch;
+using namespace std;
+
 
 class scaraBot : public hardware_interface::RobotHW
 {
 public:
-  scaraBot()
- {
-   cmd[0]=0;
-   cmd[1]=0;
-   cmd[2]=0;
-   pos[0]=0;
-   pos[1]=0;
-   pos[2]=0;
-   
-   // Connect and Register the Joint State Interface
-   hardware_interface::JointStateHandle state_handle_1("joint1", &pos[0], &vel[0], &eff[0]); 
-   jnt_state_interface.registerHandle(state_handle_1);
-
-   hardware_interface::JointStateHandle state_handle_2("joint2", &pos[1], &vel[1], &eff[1]); 
-   jnt_state_interface.registerHandle(state_handle_2);
-
-   hardware_interface::JointStateHandle state_handle_3("joint3", &pos[2], &vel[2], &eff[2]);	
-   jnt_state_interface.registerHandle(state_handle_3);
-
-   registerInterface(&jnt_state_interface);
-
-   // Connect and Register the Joint Position Interface
-   hardware_interface::JointHandle pos_handle_1(jnt_state_interface.getHandle("joint1"), &cmd[0]); 
-   jnt_pos_interface.registerHandle(pos_handle_1);
-
-   hardware_interface::JointHandle pos_handle_2(jnt_state_interface.getHandle("joint2"), &cmd[1]); 
-   jnt_pos_interface.registerHandle(pos_handle_2);
-
-   hardware_interface::JointHandle pos_handle_3(jnt_state_interface.getHandle("joint3"), &cmd[2]); 
-   jnt_pos_interface.registerHandle(pos_handle_3);
-
-   registerInterface(&jnt_pos_interface);
- }
-
-  virtual ~scaraBot()
-  {}
-
-  void write()
-  {
-	  std::cout<<"write "<<"  "<<cmd[0]<<" "<<cmd[1]<<std::endl;
-  }
-
-  void read()
-  {
-          std::cout<<"read "<<"  "<<pos[0]<<" "<<pos[1]<<std::endl;
-  }
-
+    scaraBot()
+    {
+        cmd[0]=0;
+        cmd[1]=0;
+        cmd[2]=0;
+        pos[0]=0;
+        pos[1]=0;
+        pos[2]=0;
+        eff[0]=0;
+        eff[1]=0;
+        eff[2]=0;
+        sendCommand="2,0,0,0,0,0,0,0,0\r";
+        receiveCommand="4,0,0,0,0,0,0,0,0\r";
+        
+        // Connect and Register the Joint State Interface
+        hardware_interface::JointStateHandle state_handle_1("joint1", &pos[0], &vel[0], &eff[0]); 
+        jnt_state_interface.registerHandle(state_handle_1);
+        
+        hardware_interface::JointStateHandle state_handle_2("joint2", &pos[1], &vel[1], &eff[1]); 
+        jnt_state_interface.registerHandle(state_handle_2);
+        
+        hardware_interface::JointStateHandle state_handle_3("joint3", &pos[2], &vel[2], &eff[2]);	
+        jnt_state_interface.registerHandle(state_handle_3);
+        
+        registerInterface(&jnt_state_interface);
+        
+        
+        // Connect and Register the Joint Position Interface
+        hardware_interface::JointHandle handle_1(jnt_state_interface.getHandle("joint1"), &cmd[0]); 
+        jnt_pos_interface.registerHandle(handle_1);
+        jnt_eff_interface.registerHandle(handle_1); 
+        
+        hardware_interface::JointHandle handle_2(jnt_state_interface.getHandle("joint2"), &cmd[1]); 
+        jnt_pos_interface.registerHandle(handle_2);
+        jnt_eff_interface.registerHandle(handle_2); 
+        
+        hardware_interface::JointHandle handle_3(jnt_state_interface.getHandle("joint3"), &cmd[2]); 
+        jnt_pos_interface.registerHandle(handle_3);
+        jnt_eff_interface.registerHandle(handle_3); 
+        
+        registerInterface(&jnt_pos_interface);
+        registerInterface(&jnt_eff_interface);
+    }
+    
+    virtual ~scaraBot()
+    {}
+    
+    
 private:
-  hardware_interface::JointStateInterface jnt_state_interface;
-  hardware_interface::PositionJointInterface jnt_pos_interface;
-  double cmd[3];
-  double pos[3];
-  double vel[3];
-  double eff[3];
+    hardware_interface::JointStateInterface jnt_state_interface;
+    hardware_interface::PositionJointInterface jnt_pos_interface;
+    hardware_interface::VelocityJointInterface jnt_vel_interface;
+    hardware_interface::EffortJointInterface jnt_eff_interface;
+    double cmd[3];
+    double pos[3];
+    double vel[3];
+    double eff[3];
+    string sendCommand;
+    string receiveCommand;
 };
+
+
+string scaraBot::writeR()
+{
+    
+    string cmdString="";
+    for(int i=0;i<3;i++){
+        cmdString+=to_string(cmd[i]);
+        if(i<2)
+            cmdString+=",";
+    }
+    
+    
+    cout<<"write "<<"  "<<cmd[0]<<" "<<cmd[1]<<" "<<cmd[2]<<endl;
+    int lengthString=sendCommand.length();
+    int startpoint=lengthString-5;
+    string pre=sendCommand.substr(0,startpoint);
+    // cout<<" length "<<length<<"\n";
+    // cout<<"startpoint "<<startpoint<<"\n";
+    // cout<<"prefix "<<pre<<"\n";
+    
+    //string post=to_string(a)+ "," +to_string(b)+","+to_string(c)+"\r";
+    // string c_output=pre+post;
+    // cout<<" my string : "<<c_output<<"\n";
+    
+    c_output=pre+cmdString+"\\r";
+    
+    cout<<"\n my string : "<<c_output<<endl;
+    
+    return "";
+    
+    
+}
+
+int ScaraBot::readeR()
+{
+    std::cout<<"read "<<"  "<<pos[0]<<" "<<pos[1]<<std::endl;
+}
+
+
--- sr_scara_hx_bringup/src/trial.cpp
+++ sr_scara_hx_bringup/src/trial.cpp
@@ -1,50 +1,148 @@
+#include <QCoreApplication>
 #include <iostream>
-#include<string>
-#include<sstream>
+#include <string>
+#include <sstream>
+//#include <array>
 
-using namespace std;
-//using namespace patch;
 namespace patch
 {
-    template < typename T > std::string to_string( const T& n )
-    {
-        std::ostringstream stm ;
-        stm << n ;
-        return stm.str() ;
-    }
+template < typename T > std::string to_string( const T& n )
+{
+    std::ostringstream stm ;
+    stm << n ;
+    return stm.str() ;
 }
+}
+
+
+
 using namespace patch;
+using namespace std;
+
+class Eric
+{
+public:
+    string writeR();
+    Eric()
+    {
+        a=9.876;
+        b=7.987;
+        c=0.334;
+        joint="2,0,0,0,0,0,0,70,0\r";
+
+      //cmd[]={7.00,3.88,2.22};
+        //cmd = new* float[3] = { 7.00, 3.88, 2.22};
+
+
+
+        cmd[0]=7.800;
+        cmd[1]=1.700;
+        cmd[2]=9.7700;
+
+    }
+
+private:
+
+    float a;
+    float b;
+    float c;
+    float cmd[3];
+
+    string joint;
+
+};
+
+string Eric::writeR()
+{
+ string cmdString="";
+  for(int i=0;i<3;i++){
+     cmdString+=to_string(cmd[i]);
+     if(i<2)
+       cmdString+=",";
+
+  }
+
+   cout<<"The original string is "<<joint<<endl;
+
+    //std::cout<<"write "<<"  "<<cmd[0]<<" "<<cmd[1]<<" "<<cmd[2]<<std::endl;
+    int lengthString=joint.length();
+    int startpoint=lengthString-5;
+    string pre=joint.substr(0,startpoint);
+    cout<<" length "<<lengthString<<endl;
+    cout<<"startpoint "<<startpoint<<endl;
+    cout<<"prefix "<<pre<<endl;
+
+    string post=to_string(a)+ "," +to_string(b)+","+to_string(c)+"\\r";
+    string c_output=pre+post;
+    cout<<" my string : "<<c_output<<endl;
+
+    cout<<"\n my string : "<<cmdString<<endl;
+    c_output=pre+cmdString+"\\r";
+
+    cout<<"\n my string : "<<c_output<<endl;
+
+
+
+
+    return "";
+
+}
+
 
 int main(int argc, char *argv[])
 {
 
-
-  string joint="2,0,0,0,0,0,0,70,0\r";
-  float a=100.0;
-  float b =3.190;
-  float c=9.00;
-  std::string pi = "pi is " + to_string(3.1415926);
-
- int length=joint.length();
- int startpoint=length-5;
- string pre=joint.substr(0,startpoint);
- cout<<" length "<<length<<"\n";
- cout<<"startpoint "<<startpoint<<"\n";
- cout<<"prefix "<<pre<<"\n";
-
- string post=to_string(a)+ "," +to_string(b)+","+to_string(c)+"\r";
- //string a_s=to_string(a);
- string c_output=pre+post;
- cout<<" my string : "<<c_output<<"\n";
- string last3=c_output.substr(startpoint);
- cout<<"last three digit "<<last3<<"\n";
+    Eric gamor;
+    gamor.writeR();
 
 
 
 
+    QCoreApplication a(argc, argv);
 
-
-
-
+    return a.exec();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
